b149a94270500ac8ca53d13e16fc23f989c95632
Gahaha <luke921021luke@gmail.com>
2025-05-27 03:25:00 +0800

    default things

---
+++ app/src/main/java/tw/edu/ntou/gahaha/android/androidfinalproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.ntou.gahaha.android.androidfinalproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
